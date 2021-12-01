d73486ee5c14ce6b12823cfa27ea56831258bd74
zetaemme <mattia.zorzan@gmail.com>
2021-12-01 01:31:14 +0100

    Implemented /winner API and started /ranking
    By coding /ranking correctly we can make /winner much shorter

--- Homework4/rest-championship-template/src/main/java/it/univr/championship/ChampionshipController.java
+++ Homework4/rest-championship-template/src/main/java/it/univr/championship/ChampionshipController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
@@ -53,6 +52,103 @@ public class ChampionshipController {
 	public Map<String, Iterable<Match>> getAllMatchesOfTeam(@PathVariable("teamName") String teamName) {
 		Map<String, Iterable<Match>> res = new HashMap<>();
 		res.put("matches", matchRepository.findAllByTeamAOrTeamB(teamName, teamName));
+
+		return res;
+	}
+
+	@GetMapping("/ranking")
+	public Map<String, String> getRanking() {
+		Map<String, Integer> teams = new HashMap<>();
+
+		for (String team : getAllTeams().get("teams")) {
+			teams.put(team, 0);
+		}
+
+		Match.MatchResult result;
+
+		for (Match m : getMatches().get("matches")) {
+			result = m.computeResult();
+
+			if (result == Match.MatchResult.A_WINNER) {
+				teams.replace(m.getTeamA(), teams.get(m.getTeamA()) + 3);
+			} else if (result == Match.MatchResult.B_WINNER) {
+				teams.replace(m.getTeamB(), teams.get(m.getTeamB()) + 3);
+			} else {
+				teams.replace(m.getTeamA(), teams.get(m.getTeamA()) + 1);
+				teams.replace(m.getTeamB(), teams.get(m.getTeamB()) + 1);
+			}
+		}
+
+		Map<String, Integer> sortedTeams = teams.entrySet().stream().sorted(
+				Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(
+				Collectors.toMap(
+						Map.Entry::getKey,
+						Map.Entry::getValue,
+						(oldValue, newValue) -> oldValue, LinkedHashMap::new
+				)
+		);
+
+		Map<String, String> res = new HashMap<>();
+
+		// FIXME Da print il risultato è corretto ma quando esegue il test il valore è null
+		int i = 1;
+		for (String teamName : sortedTeams.keySet()) {
+			res.put(i + ".team", teamName);
+			res.put(i + ".points", sortedTeams.get(teamName).toString());
+
+			// System.out.println(i + ".team: " + teamName);
+			// System.out.println(i + ".points: " + sortedTeams.get(teamName));
+
+			i++;
+		}
+
+		return res;
+	}
+
+	@GetMapping("/winner")
+	public Map<String, String> getWinner() {
+		Map<String, Integer> teams = new HashMap<>();
+
+		for (String team : getAllTeams().get("teams")) {
+			teams.put(team, 0);
+		}
+
+		Match.MatchResult result;
+
+		for (Match m : getMatches().get("matches")) {
+			result = m.computeResult();
+
+			if (result == Match.MatchResult.A_WINNER) {
+				teams.replace(m.getTeamA(), teams.get(m.getTeamA()) + 3);
+			} else if (result == Match.MatchResult.B_WINNER) {
+				teams.replace(m.getTeamB(), teams.get(m.getTeamB()) + 3);
+			} else {
+				teams.replace(m.getTeamA(), teams.get(m.getTeamA()) + 1);
+				teams.replace(m.getTeamB(), teams.get(m.getTeamB()) + 1);
+			}
+		}
+
+		Map<String, String> res = new HashMap<>();
+
+		res.put(
+				"winner",
+				Collections.max(teams.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey()
+		);
+
+		return res;
+	}
+
+	@GetMapping("/teams")
+	public Map<String, Iterable<String>> getAllTeams() {
+		Map<String, Iterable<String>> res = new HashMap<>();
+		Set<String> teams = new HashSet<>();
+
+		matchRepository.findAll().forEach(match -> {
+			teams.add(match.getTeamA());
+			teams.add(match.getTeamB());
+		});
+
+		res.put("teams", teams);
 
 		return res;
 	}
